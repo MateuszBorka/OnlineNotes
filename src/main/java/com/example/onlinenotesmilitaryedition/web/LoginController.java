bbef5e986e0cd43c89ad0a64e5d7f131ae744913
MateuszBorka <borko.m.126@gmail.com>
2023-02-13 00:35:14 +0100

    added correct validation while registering

--- src/main/java/com/example/onlinenotesmilitaryedition/web/LoginController.java
+++ src/main/java/com/example/onlinenotesmilitaryedition/web/LoginController.java
@@ -2,6 +2,7 @@ package com.example.onlinenotesmilitaryedition.web;
 
 import com.example.onlinenotesmilitaryedition.dao.UserService;
 import com.example.onlinenotesmilitaryedition.models.User;
+import com.example.onlinenotesmilitaryedition.validators.UserValidator;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,17 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+
 
 @Controller
 public class LoginController {
 
 
     private final UserService userService;
+    private final UserValidator userValidator;
 
     @Autowired
-    public LoginController(UserService userService) {
+    public LoginController(UserService userService, UserValidator userValidator) {
         this.userService = userService;
+        this.userValidator = userValidator;
     }
+
+
 
 
     @GetMapping("/")
@@ -49,18 +57,21 @@ public class LoginController {
         return "registrationPage";
     }
 
+
+
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute @NotNull User user, Model model) {
-        User existingUser = userService.findByUsername(user.getUsername());
-        if (existingUser == null) {
+    public String registerUser(@ModelAttribute @Valid User user, BindingResult result, Model model) {
+        userValidator.validate(user, result);
+
+        if (result.hasErrors()) {
+            model.addAttribute("message", "Username already exists");
+            return "registrationPage";
+        } else {
             // Save the new user in the database
             userService.save(user);
             // Redirect to the login page
             return "redirect:/";
-        } else {
-            // Show error message
-            model.addAttribute("message", "Username already exists");
-            return "registrationPage";
+
         }
     }
 
